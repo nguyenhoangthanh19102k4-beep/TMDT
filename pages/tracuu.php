f6d5c0939b566178916972ffe11a38b2d58243dd
haman-1608 <haminhkieuman@gmail.com>
2026-01-09 21:01:22 +0700

    update ui tracuu

--- pages/tracuu.php
+++ pages/tracuu.php
@@ -20,39 +20,9 @@
     <form action="">
         <input placeholder="Nhập mã đơn hàng" type="text" name="order-code">
         <input placeholder="Nhập số điện thoại/Email" type="text" name="order-number-or-email">
+        <button type="submit" name="search-order">
+            TRA CỨU
+        </button>
     </form>
-
-    <div class="dichvu"> 
-        <div class="dvu1">
-            <div>
-            <img src="imgs/trangchu/mdi--support 2.svg" alt="" class="">
-            <p> <b>Vệ Sinh Kính Miễn Phí</b> <br>
-            tại toàn bộ hệ thống mắt kính Good Optic
-            </p>
-            </div>
-            <div>
-            <img src="imgs/trangchu/carbon--delivery 1.svg" alt="" class="">
-            <p> <b >Giao Hàng Nhanh</b><br>
-            chỉ từ 2 ngày trên toàn quốc
-            </p>
-            </div>
-        </div>
-        
-        <div class="dvu1"> 
-            <div>
-            <img src="imgs/trangchu/carbon--ibm-data-product-exchange 1.svg" alt="" class="">
-            <p> <b>Thu Cũ Đổi Mới </b><br>
-            trợ giá lên đến 200.000đ
-            </p>
-            </div>
-            <div>
-            <img src="imgs/trangchu/image 1.svg" alt="" class="">
-            <p> <b>Hỗ Trợ Đo Mắt</b> <br>
-            tại toàn bộ hệ thống mắt kính Good Optic
-            </p>
-            </div>
-        </div>
-        
-    </div>
-</div>
+ </div>
  
